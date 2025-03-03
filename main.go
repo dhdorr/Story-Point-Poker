273f4c6c1522163fb3758f9dc7ac509ac6bdc2fa
Derek Dorr <dhdorr@gmail.com>
2025-03-02 23:13:32 -0800

    sessions and skins
    added the ability to join or create sessions as well as using custom skins for the poker table (regular colors)

--- main.go
+++ main.go
@@ -14,19 +14,107 @@ type Deck struct {
 	Cards []Card
 }
 
+type Table_Template struct {
+	Deck      []Card
+	Skin      string
+	TextColor string
+}
+
 type Session struct {
 	ID          string
 	Players     int
 	CurrentDeck Deck
+	Passcode    string
+	Closed      bool
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, d *Deck) {
+func NewSession(id, passcode string, pCount int, closed bool, deck Deck) *Session {
+	return &Session{ID: id, Passcode: passcode, Players: pCount, Closed: closed, CurrentDeck: deck}
+}
+
+type Poker_Tables struct {
+	active_sessions map[string]Session
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, ts *Table_Template) {
 	t, _ := template.ParseFiles("./templates/" + tmpl + ".html")
-	t.Execute(w, d)
+	t.Execute(w, ts)
+}
+
+func (poker *Poker_Tables) handle_test(w http.ResponseWriter, _ *http.Request) {
+	data := Deck{
+		Cards: []Card{
+			{Value: 1},
+			{Value: 2},
+			{Value: 3},
+			{Value: 4},
+			{Value: 5},
+		},
+	}
+	ts := Table_Template{Deck: data.Cards, Skin: "grey", TextColor: "black"}
+	renderTemplate(w, "card", &ts)
+}
+
+func (poker *Poker_Tables) handle_join(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Query())
+	session_id := r.URL.Query().Get("sessionID")
+	passcode := r.URL.Query().Get("passcode")
+	skin := r.Header.Get("bg_skin")
+	text_color := r.Header.Get("bg_text")
+
+	error_message := "SessionID or Passcode does not match"
+
+	val, ok := poker.active_sessions[session_id]
+	if !ok {
+		fmt.Printf("No session with ID: %s", session_id)
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, error_message)
+		return
+	}
+
+	if val.Passcode != passcode {
+		fmt.Printf("Passcode does not match for sessionID: %s", session_id)
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, error_message)
+		return
+	}
+	// renderTemplate(w, "test2", nil)
+	ts := Table_Template{Deck: val.CurrentDeck.Cards, Skin: skin, TextColor: text_color}
+	renderTemplate(w, "card", &ts)
+}
+
+func (poker *Poker_Tables) handle_create(w http.ResponseWriter, r *http.Request) {
+	session_id := r.FormValue("sessionID")
+	preset := r.FormValue("preset")
+	passcode := r.FormValue("passcode")
+	skin := r.Header.Get("bg_skin")
+	text_color := r.Header.Get("bg_text")
+
+	fmt.Printf("Session ID: %s | Preset: %s | Passcode: %s", session_id, preset, passcode)
+
+	cards := make([]Card, 0, 8)
+
+	if preset == "seq" {
+		for i := 0; i < 8; i++ {
+			cards = append(cards, Card{Value: i + 1})
+		}
+	} else if preset == "fib" {
+		fib := []int{1, 2, 3, 5, 8, 13, 21, 34}
+		for i := 0; i < 8; i++ {
+			cards = append(cards, Card{Value: fib[i]})
+		}
+	}
+
+	data := Deck{
+		Cards: cards,
+	}
+	poker.active_sessions[session_id] = *NewSession(session_id, passcode, 0, false, data)
+	ts := Table_Template{Deck: data.Cards, Skin: skin, TextColor: text_color}
+	renderTemplate(w, "card", &ts)
 }
 
 func main() {
-	active_sessions := make(map[string]Session)
+	poker_tables := &Poker_Tables{active_sessions: make(map[string]Session)}
 
 	// Test Values
 	data := Deck{
@@ -38,63 +126,18 @@ func main() {
 			{Value: 5},
 		},
 	}
-	active_sessions["abc123"] = Session{ID: "abc123", Players: 0, CurrentDeck: data}
+	poker_tables.active_sessions["abc123"] = Session{ID: "abc123", Players: 0, CurrentDeck: data, Passcode: "test", Closed: false}
 	// End Test
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index2.html")
 	})
 
-	http.HandleFunc("GET /test", func(w http.ResponseWriter, r *http.Request) {
-		data := Deck{
-			Cards: []Card{
-				{Value: 1},
-				{Value: 2},
-				{Value: 3},
-				{Value: 4},
-				{Value: 5},
-			},
-		}
-		renderTemplate(w, "card", &data)
-	})
+	http.HandleFunc("GET /test", poker_tables.handle_test)
 
-	http.HandleFunc("GET /join", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.URL.Query())
-		session_id := r.URL.Query().Get("sessionID")
-		val, ok := active_sessions[session_id]
-		if !ok {
-			fmt.Printf("No session with ID: %s", session_id)
-			w.WriteHeader(404)
-			fmt.Fprintf(w, "No session found with ID: %s", session_id)
-			return
-		}
-		// renderTemplate(w, "test2", nil)
-		renderTemplate(w, "card", &val.CurrentDeck)
-	})
+	http.HandleFunc("GET /join", poker_tables.handle_join)
 
-	http.HandleFunc("POST /create", func(w http.ResponseWriter, r *http.Request) {
-		session_id := r.FormValue("sessionID")
-		preset := r.FormValue("preset")
-		fmt.Printf("Session ID: %s | Preset: %s", session_id, preset)
-
-		cards := []Card{}
-
-		if preset == "seq" {
-			for i := 0; i < 8; i++ {
-				cards = append(cards, Card{Value: i + 1})
-			}
-		} else if preset == "fib" {
-			fib := []int{1, 2, 3, 5, 8, 13, 21, 34}
-			for i := 0; i < 8; i++ {
-				cards = append(cards, Card{Value: fib[i]})
-			}
-		}
-
-		data := Deck{
-			Cards: cards,
-		}
-		renderTemplate(w, "card", &data)
-	})
+	http.HandleFunc("POST /create", poker_tables.handle_create)
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
--- static/scripts/select-card.js
+++ static/scripts/select-card.js
@@ -1,3 +1,6 @@
+let bg_skin = "grey";
+let bg_text = "black";
+
 function choose(card) {
     console.log("clicked...");
     if (card.classList.contains("selected")) {
@@ -9,7 +12,7 @@ function choose(card) {
 
 document.addEventListener("fx:config", (evt) => {
     evt.detail.cfg.headers.testme = "don't panic";
-    console.log("After headers: " + evt.detail.cfg.body.get("sessionID"));
+    console.log("After headers: " + evt.detail.cfg.body);
 })
 
 function logEvt(evt) {
@@ -19,7 +22,8 @@ function logEvt(evt) {
 }
 
 function handleErrorResponse(evt) {
-    if (evt.detail.cfg.response.status == 404) {
+    let error_arr = [401, 404];
+    if (error_arr.includes(evt.detail.cfg.response.status)) {
         console.log("there was a 404 error...")
         // document.head.remove();
         let error_dialog = document.getElementById("errorDialog");
@@ -28,7 +32,38 @@ function handleErrorResponse(evt) {
     }
 }
 
+function handleRequest(evt) {
+    evt.detail.cfg.headers.bg_skin = bg_skin;
+    evt.detail.cfg.headers.bg_text = bg_text;
+    console.log("testing before req: ", evt.detail.cfg);
+}
+
 document.addEventListener("fx:after", (evt) => handleErrorResponse(evt))
+document.addEventListener("fx:before", (evt) => handleRequest(evt))
 
+// let radios = document.getElementsByName("skin");
 
+// radios.forEach(rad => {
+//     rad.addEventListener("change", function() {
+//         console.log("changing color to " + this.value);
+//         if (this.checked) {
+//             console.log("changing color to " + this.value);
+//         }
+//     })
+// })
 
+function swap_skin(radio) {
+    console.log("changing color to " + radio.value);
+    let bg = document.getElementById("funBG");
+    if (radio.value == "rainbow") {
+        bg.style.background = "linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,154,0,1) 10%, rgba(208,222,33,1) 20%, rgba(79,220,74,1) 30%, rgba(63,218,216,1) 40%, rgba(47,201,226,1) 50%, rgba(28,127,238,1) 60%, rgba(95,21,242,1) 70%, rgba(186,12,248,1) 80%, rgba(251,7,217,1) 90%, rgba(255,0,0,1) 100%)";
+        bg.style.color = "black";
+    } else {
+        bg.style.background = null;
+        bg.style.backgroundColor = radio.value;
+    }
+    bg.style.color = radio.getAttribute("data-color");
+
+    bg_skin = radio.value;
+    bg_text = radio.getAttribute("data-color");
+}
