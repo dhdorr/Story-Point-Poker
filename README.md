64a6ba7fd1c16745fd6b01436fd16283a77f7eae
Derek Dorr <dhdorr@gmail.com>
2025-03-11 23:25:27 -0700

    reorganize

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Story-Point-Poker
  story point poker web app to support multiple lobbies of people placing bets on story point estimates. Use as a tool for AGILE sprint planning.
+
+
+## Folder Structure:
+- templates: is for html markup that may be dynamically rendered by golang's templating
+- static: is for all unchanging assets like css, javascript, and static web pages
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -14,6 +15,20 @@ func main() {
 	fs_pages := http.FileServer(http.Dir("pages"))
 	http.HandleFunc("GET /pages/", func(w http.ResponseWriter, r *http.Request) {
 		http.StripPrefix("/pages/", fs_pages).ServeHTTP(w, r)
+	})
+
+	// serve dynamic html templates
+	fs_templates := http.FileServer(http.Dir("templates"))
+	http.HandleFunc("GET /templates/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.RequestURI)
+		http.StripPrefix("/templates/", fs_templates).ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("POST /joinTable", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		fmt.Printf("joining table: %v \n", r.Form)
+		tmpl := template.Must(template.ParseFiles("templates/T-poker-table.html"))
+		tmpl.Execute(w, nil)
 	})
 
 	// serve css and js, from html pages
